627a8376cf078bdb95e28ffe2ecb85bd7438b85f
Alexander Roy <alexanderoroy@gmail.com>
2018-06-12 13:29:31 -0400

    Make home page full of ipsums

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -7,7 +7,7 @@ class SentencesController < ApplicationController
   @sentence = Sentence.new(sentence_params)
     if @sentence.save
       flash[:info] = "Sentence Added!"
-      redirect_to root_url
+      redirect_to new_sentence_url
     else
       flash[:info] = "Didn't create sentence"
       redirect_to new_sentence_url
@@ -26,7 +26,19 @@ class SentencesController < ApplicationController
       redirect_to '/404.html'
   end
 
+  def home
+    @sentences = Sentence.order("RANDOM()").limit(100).pluck(:content)
+    fill_it_up
+    @sentences.shuffle!
+  end
+
   private
+
+  def fill_it_up
+    while @sentences.count.between?(1,99) do
+      @sentences += @sentences
+    end
+  end
 
     def sentence_params
       params.require(:sentence).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'sentences#index'
-  resources :sentences
+  root 'sentences#home'
+  resources :sentences, only: [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
