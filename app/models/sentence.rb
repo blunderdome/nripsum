c1b7f88e271f391f77eb23ea244939c933f7c538
Alexander Roy <alexanderoroy@gmail.com>
2018-07-03 15:27:59 -0400

    add end punctuation is missing

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,15 @@
 class Sentence < ActiveRecord::Base
+  before_save :add_end_punctuation_if_missing
+
+  VALID_END_PUNCTUATION = %w(. ! ?)
+
+  END_PUNCTUATION_TO_APPEND = %w(. ! ? !? ?! ?? !!)
+
+  def add_end_punctuation_if_missing
+    if self.content.chars.last.in?(VALID_END_PUNCTUATION)
+      # do nothing
+    else
+      self.content << END_PUNCTUATION_TO_APPEND.sample
+    end
+  end
 end
